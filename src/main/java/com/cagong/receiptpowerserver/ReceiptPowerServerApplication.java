c550a78c8cac2bf0f31fa6d4aadee927d3ee540a
YuYeonho <yuyeonho0619@gmail.com>
2025-05-19 18:33:05 +0900

    첫번째 커밋

---
+++ src/main/java/com/cagong/receiptpowerserver/ReceiptPowerServerApplication.java
@@ -0,0 +1,13 @@
+package com.cagong.receiptpowerserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReceiptPowerServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReceiptPowerServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cagong/receiptpowerserver/ReceiptPowerServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cagong.receiptpowerserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReceiptPowerServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
