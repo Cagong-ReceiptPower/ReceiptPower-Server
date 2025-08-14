b80a766dfad1e8c4bb2bb06aaad4b28bb45de33a
YuYeonho <127312366+yuuuyeonho@users.noreply.github.com>
2025-08-14 16:01:37 +0900

    feat:도커 설정 (#8)
    * fix: 구조 수정
    
    * feat: 도커 설정
    
    * feat: env 파일 설정
    
    * chore: docker compose
    
    * chore: CI 파일 정리
    
    * feat: 깃허브액션 CD 파일 작성
    
    * test: test
    
    * fix: ssh-action 버전 수정
    
    * fix: cd스크립트
    
    * fix: cd스크립트
    
    * chore: 브랜치 다시 main으로 되돌림

--- src/main/java/com/cagong/receiptpowerserver/domain/member/MemberController.java
+++ src/main/java/com/cagong/receiptpowerserver/domain/member/MemberController.java
@@ -2,9 +2,7 @@ package com.cagong.receiptpowerserver.domain.member;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
--- src/main/java/com/cagong/receiptpowerserver/domain/member/MemberService.java
+++ src/main/java/com/cagong/receiptpowerserver/domain/member/MemberService.java
@@ -1,7 +1,9 @@
 package com.cagong.receiptpowerserver.domain.member;
 
+import com.cagong.receiptpowerserver.domain.member.Member;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
