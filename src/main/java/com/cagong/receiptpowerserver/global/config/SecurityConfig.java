30731517c519c3d89017a75df7692d848190683f
YuYeonho <yuyeonho0619@gmail.com>
2025-09-13 16:52:55 +0900

    chore:오타 수정

--- src/main/java/com/cagong/receiptpowerserver/global/config/SecurityConfig.java
+++ src/main/java/com/cagong/receiptpowerserver/global/config/SecurityConfig.java
@@ -4,10 +4,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-<<<<<<< HEAD
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-=======
->>>>>>> main
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
