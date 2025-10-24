af28941fe09f53f75f1859b05bda8895b9d4b4b1
YuYeonho <127312366+yuuuyeonho@users.noreply.github.com>
2025-10-24 17:24:45 +0900

    feat: mqtt 호출 (#27)

---
+++ src/main/java/com/cagong/receiptpowerserver/global/config/MqttConfig.java
@@ -0,0 +1,48 @@
+package com.cagong.receiptpowerserver.global.config;
+
+import org.eclipse.paho.client.mqttv3.persist.MemoryPersistence;
+import org.springframework.beans.factory.annotation.Value;
+import lombok.extern.slf4j.Slf4j;
+import org.eclipse.paho.client.mqttv3.MqttClient;
+import org.eclipse.paho.client.mqttv3.MqttConnectOptions;
+import org.eclipse.paho.client.mqttv3.MqttException;
+import org.eclipse.paho.client.mqttv3.persist.MqttDefaultFilePersistence;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+@Profile("!test")
+@Configuration
+@Slf4j
+public class MqttConfig {
+
+    @Value("${mqtt.broker}")
+    private String brokerUrl;
+
+    @Value("${mqtt.username}")
+    private String username;
+
+    @Value("${mqtt.password}")
+    private String password;
+
+    @Value("${mqtt.client-id}")
+    private String clientId;
+
+    @Bean
+    public MqttClient mqttClient() throws MqttException {
+        // broker URL 은 반드시 ssl:// 로 시작해야 함 (TLS)
+        MqttClient client = new MqttClient(brokerUrl, clientId, new MemoryPersistence());
+
+        MqttConnectOptions options = new MqttConnectOptions();
+        options.setUserName(username);
+        options.setPassword(password.toCharArray());
+        options.setCleanSession(true);
+        options.setAutomaticReconnect(true);
+        options.setConnectionTimeout(10);
+        options.setKeepAliveInterval(30);
+
+        client.connect(options);
+        log.info("✅ Connected to HiveMQ Cloud broker at {}", brokerUrl);
+        return client;
+    }
+}
