982e538f0600948a7198a317881b58971e323fde
diacond <tiger874587@gmail.com>
2025-11-09 12:12:54 +0900

    renew

--- src/test/java/com/cagong/receiptpowerserver/MemberSignupTest.java
+++ src/test/java/com/cagong/receiptpowerserver/MemberSignupTest.java
@@ -12,7 +12,6 @@ import org.springframework.test.context.ActiveProfiles;
 import org.springframework.transaction.annotation.Transactional;
 
 import static org.assertj.core.api.Assertions.*;
-import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 @ActiveProfiles("test")
