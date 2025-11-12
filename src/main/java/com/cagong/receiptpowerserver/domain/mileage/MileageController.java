a892e3030890b92acadef820173bc68ed9725829
YuYeonho <127312366+yuuuyeonho@users.noreply.github.com>
2025-11-12 13:52:22 +0900

    refactor: 마일리지 관련 리팩토링 (#31)

--- src/main/java/com/cagong/receiptpowerserver/domain/mileage/MileageController.java
+++ src/main/java/com/cagong/receiptpowerserver/domain/mileage/MileageController.java
@@ -1,6 +1,7 @@
 package com.cagong.receiptpowerserver.domain.mileage;
 
 import com.cagong.receiptpowerserver.domain.mileage.dto.*;
+import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -13,6 +14,7 @@ public class MileageController {
     private final MileageService mileageService;
 
     @GetMapping("/total")
+    @Operation(summary = "전체 마일리지 조회")
     public ResponseEntity<TotalMileageResponse> getTotalMileage(){
         try {
             TotalMileageResponse response = mileageService.getTotalMileage();
@@ -23,6 +25,7 @@ public class MileageController {
     }
 
     @GetMapping
+    @Operation(summary = "개별 마일리지 조회")
     public ResponseEntity<CafeMileageResponse> getCafeMileage(@RequestParam Long cafeId){
         try {
             CafeMileageResponse response = mileageService.getCafeMileage(cafeId);
@@ -33,6 +36,7 @@ public class MileageController {
     }
 
     @PostMapping
+    @Operation(summary = "마일리지 적립")
     public ResponseEntity<SaveMileageResponse> saveMileage(@RequestBody SaveMileageRequest request){
         try {
             SaveMileageResponse response = mileageService.addMileage(request);
@@ -43,16 +47,18 @@ public class MileageController {
     }
 
     @PostMapping("/{cafeId}/usage")
-    public ResponseEntity<Void> useMileage(@PathVariable Long cafeId){
+    @Operation(summary = "마일리지 사용 시작")
+    public ResponseEntity<UseMileageResponse> useMileage(@PathVariable Long cafeId){
         try {
-            mileageService.startMileageUsage(cafeId);
-            return ResponseEntity.ok().build();
+            UseMileageResponse response = mileageService.startMileageUsage(cafeId);
+            return ResponseEntity.ok().body(response);
         } catch (IllegalArgumentException e) {
             return ResponseEntity.badRequest().build();
         }
     }
 
     @PostMapping("/{cafeId}/usage/end")
+    @Operation(summary = "마일리지 사용 끝")
     public ResponseEntity<EndMileageUsageResponse> endMileage(@PathVariable Long cafeId){
         try {
             EndMileageUsageResponse response = mileageService.endMileageUsage(cafeId);
