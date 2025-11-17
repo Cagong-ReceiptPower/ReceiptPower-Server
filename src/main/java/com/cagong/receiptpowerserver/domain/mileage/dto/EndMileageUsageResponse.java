9ad6bcf0a1a10ff2adb185b4cc03b445c6715155
YuYeonho <127312366+yuuuyeonho@users.noreply.github.com>
2025-11-17 19:33:42 +0900

    fix: 스웨거 충돌나서 스프링부트 버전 수정 (#37)

--- src/main/java/com/cagong/receiptpowerserver/domain/mileage/dto/EndMileageUsageResponse.java
+++ src/main/java/com/cagong/receiptpowerserver/domain/mileage/dto/EndMileageUsageResponse.java
@@ -2,9 +2,11 @@ package com.cagong.receiptpowerserver.domain.mileage.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
-@AllArgsConstructor
 @Getter
+@AllArgsConstructor
+@NoArgsConstructor
 public class EndMileageUsageResponse {
     private int remainingMileage;
 }
